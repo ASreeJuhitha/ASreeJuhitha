945d896a6439b86677dc2184c92188706fddeee2
ASreeJuhitha <sreejuhithaannadevara2017@gmail.com>
2023-10-19 15:30:01 +0530

    maven project to git assignment5

---
+++ jenkinsAssignment5/src/main/java/com/example/jenkinsAssignment5/JenkinsAssignment5Application.java
@@ -0,0 +1,14 @@
+package com.example.jenkinsAssignment5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsAssignment5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsAssignment5Application.class, args);
+		System.out.println("Sucessfully executed jenkinsAssignment5!!!");
+	}
+
+}
---
+++ jenkinsAssignment5/src/test/java/com/example/jenkinsAssignment5/JenkinsAssignment5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsAssignment5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsAssignment5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
